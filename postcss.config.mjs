8ba8de16a2735ffe35afc7b5f6f0a5766fb2ac03
ankitku3101 <ankit.ku3101@gmail.com>
2025-03-06 22:11:45 +0530

    Updated Hero.tsx

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,4 @@
 const config = {
-  theme:{
-    extend:{
-      colors:{
-        primary:"#fffce1",
-        dark:"#010002",
-        green:"#4ade80",
-      }
-    }
-  },
   plugins: ["@tailwindcss/postcss"],
 };
 
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,32 +1,29 @@
 import React from 'react'
+import localFont from 'next/font/local'
+
+const myFont = localFont({ src: '../styles/fonts/RapierZeroItalic.otf' })
 
 const Hero = () => {
   return (
     <div 
-    className='bg-center h-screen w-full flex items-center justify-center bg-[#180126]/78 '
-    style={
-        {
-            backgroundImage:`url(${'/Hero.jpg'})`,
-            backgroundSize:'cover',
-            height:"100vh",
-            width:"100vw",
-            backgroundPosition:"center",
-            backgroundRepeat:'no-repeat',
-            backgroundBlendMode: 'overlay',
+        className='bg-center min-h-screen w-full flex items-center justify-center bg-[#180126]/90 '
+        style={
+            {
+                backgroundImage:`url(${'/Hero.jpg'})`,
+                backgroundSize:'cover',
+                width:"100vw",
+                backgroundPosition:"center",
+                backgroundRepeat:'no-repeat',
+                backgroundBlendMode: 'overlay',
+            }
         }
-    }>
+    >
         <div>
             <div 
-            className=' text-light font-bold'
-            style={{
-                fontSize: 'clamp(3rem, 9vw, 9rem)', // Scales between mobile and large screens
-                textAlign: 'center'
-            }}
+            className='bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 p-8'
             >
-                FESTRONIX
-            </div>
-            <div className='text-center text-light text-4xl'>
-                2025
+                <h1 className={`${myFont.className} text-6xl md:text-7xl lg:text-9xl tracking-wide`}>FESTR<span className='text-green-400'>O</span>N<span className='text-green-400'>I</span>X</h1>
+                <p className={`${myFont.className} text-[37px] md:text-[42px] lg:text-[85px] tracking-wider text-right mr-9 -mt-5 md:mr-11 md:-mt-6 lg:mr-18 lg:-mt-13`}>2<span className='text-green-400'>0</span>25</p>
             </div>
         </div>
     </div>
