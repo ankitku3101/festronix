2bfc950a2e6becd81c6496925c41def721967cf2
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-06 18:52:12 +0530

    Hero Page settting up to added opacity.

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,18 +3,29 @@ import React from 'react'
 const Hero = () => {
   return (
     <div 
-    className='flex items-center justify-center min-h-screen'
+    className='bg-center h-screen w-full flex items-center justify-center bg-[#180126]/78 '
     style={
         {
             backgroundImage:`url(${'/Hero.jpg'})`,
-            
+            backgroundSize:'cover',
+            height:"100vh",
+            width:"100vw",
+            backgroundPosition:"center",
+            backgroundRepeat:'no-repeat',
+            backgroundBlendMode: 'overlay',
         }
     }>
-        <div >
-            <div className=' text-light font-bold text-7xl'>
+        <div>
+            <div 
+            className=' text-light font-bold'
+            style={{
+                fontSize: 'clamp(3rem, 9vw, 9rem)', // Scales between mobile and large screens
+                textAlign: 'center'
+            }}
+            >
                 FESTRONIX
             </div>
-            <div className='text-center text-light font-bold' style={{ letterSpacing: '0.7em' }}>
+            <div className='bg-amber-200 text-center text-light text-4xl' style={{ letterSpacing: '1.0em' }}>
                 2025
             </div>
         </div>
