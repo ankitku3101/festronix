39e523b9ffc8e4e4585c7c495153a18d4b39b8fd
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-09 12:48:44 +0530

    Type interface is modified with doc link.

--- types/TypeFile.tsx
+++ types/TypeFile.tsx
@@ -3,10 +3,17 @@ export interface indepthDetails {
     points: string[];
   }
   
-  export interface Detail {
-    image: string;
-    title: string;
-    description: string;
-    specification?: indepthDetails[];
-  }
-  
+export interface Detail {
+  image: string;
+  title: string;
+  description: string;
+  specification?: indepthDetails[];
+  doclink?:string;
+}
+
+export interface LearnMoreProps {
+  doclink: string
+  children?: React.ReactNode
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link"
+  size?: "default" | "sm" | "lg" | "icon"
+}
