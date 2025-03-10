83395061d746832cd10c6f013d0f5969cecf4be3
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-10 13:17:51 +0530

    Responsiveness of Component Handled.

--- components/event-info/EventDetails.tsx
+++ components/event-info/EventDetails.tsx
@@ -9,7 +9,7 @@ interface DetailProps{
 
 const EventDetails : React.FC<DetailProps>  = ({details,odd}) => {
   return (
-    <div className='space-y-20'>
+    <div className='space-y-12 md:space-y-20 w-full'>
     {
         details.map((item,index)=>{
             return <CardNText key={index+(odd?1:0)} index={index+(odd?1:0)} image={item.image} description={item.description} title={item.title} specification={item.specification} doclink={item.doclink}/>
