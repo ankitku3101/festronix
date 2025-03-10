a4cdc7677253a5c5d409b502e5836e8f75befe2c
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-11 00:54:33 +0530

    AIML Contest, IoT innovation challenge, Mobile App development, Web Design link uploaded with final doc.

--- sections/TechnicalEvents.tsx
+++ sections/TechnicalEvents.tsx
@@ -4,31 +4,33 @@ import {Detail,indepthDetails} from "@/types/TypeFile"
 
 const details : Detail[] = [
     { 
-        image: "/hero1.jpg", 
+        image: "/techevents/hackathon1.jpg", 
         title: "Hackathon", 
         description: "A 24-hour coding showdown where participants solve real-world problems, fostering innovation, teamwork, and technical excellence." 
     },
     { 
-        image: "/hero1.jpg", 
+        image: "/techevents/aiml.jpg", 
         title: "AI & ML Model Contest", 
         description: "A fast-paced competition where developers and data scientists come together to showcase their build innovative AI-ML models.",
-        doclink:"https://drive.google.com/file/d/1py0VvcyFOmLnnkEXXYrMHNlsbGn7nAcD/view?usp=sharing",
+        doclink:"https://drive.google.com/file/d/1YIpvAh8p8tXOSLLgFQjWabl6qCWACJZO/view?usp=sharing",
     },
     { 
-        image: "/hero1.jpg", 
+        image: "/techevents/webdesign1.jpg", 
         title: "Web Design Contest", 
-        description: "A creative platform for building visually appealing and functional websites, focusing on UI/UX, design principles, and coding skills." 
+        description: "A creative platform for building visually appealing and functional websites, focusing on UI/UX, design principles, and coding skills.",
+        doclink:"https://drive.google.com/file/d/1ymHcfPEabGZpNT8TfpFl4P_DaXX0MAhq/view?usp=sharing"
     },
     { 
-        image: "/hero1.jpg", 
+        image: "/techevents/mobileapp1.jpg", 
         title: "Mobile App Development Challenge", 
         description: "A fast-paced hackathon where developers and designers come together to build innovative mobile apps within 4 hours! Participants can work solo or in teams, using Android frameworks.",
-        doclink:'https://drive.google.com/file/d/1Awv3wcjQkfOhBHrxW07EkCD033LOgrKP/view?usp=sharing'
+        doclink:'https://drive.google.com/file/d/1Y_n2TciJPVqWCYmhkal2_6F253TAw4so/view?usp=sharing'
     },
     { 
-        image: "/hero1.jpg", 
+        image: "/techevents/iot.jpg", 
         title: "IoT Innovation Challenge", 
-        description: "A contest to develop IoT-based prototypes addressing real-world problems, integrating sensors, automation, and networking."
+        description: "Turn your ideas into reality! This challenge isn't just about building smart solutions — it's about creating innovations with real-world impact. The most promising ideas have the potential to be filed for patents, with full support from the organizing team for documentation, legal procedures, and filing. Selected participants will also receive expert mentorship on protecting and commercializing their innovations. If you dream of making a mark in the tech world, this is your chance!",
+        doclink:"https://drive.google.com/file/d/1P6Ue-oy_ZOrjou_2zvohSUNtP7kJAssV/view?usp=sharing"
     }
 ];
 
