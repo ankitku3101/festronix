5878b5546f2b41810ad25e280c05ddff20a193c8
royanish123 <anish.anandroy@gmail.com>
2025-03-11 09:58:33 +0530

    Updated Register.tsx

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -6,7 +6,7 @@ function Register() {
     <div className='min-h-screen flex justify-center text-light p-4 md:p-8 lg:p-20 mt-10'>
         <div className='w-full max-w-7xl'>
             <h1 className='text-center mb-12 text-4xl md:text-5xl lg:text-6xl font-bold'>
-                Registeration
+                Registration
             </h1>
             <p className='text-center m-8 text-xl md:text-2xl lg:text-3xl '>
                 Follow these steps to register for the events at Festronix 2025
